b65f1e249d7413b8cf07b025dd316f975cfd9435
PedroDardoRamonCayo <pedrodardoramoncayo@gmail.com>
2025-04-27 14:59:49 -0300

    Inserccion Del Arreglo 1, PedroCayo

--- tp03PV2025Grupo22/src/assets/components/App2.jsx
+++ tp03PV2025Grupo22/src/assets/components/App2.jsx
@@ -1,16 +1,15 @@
 //Agregar aquí todos los import necesarios.
 
 
-
-
 function App2(){
-    const nombreMateria = "Programacion Visual";
+  const nombreMateria = "Programacion Visual";
+  
 return(
-    <div>
-        <h1>Hola React </h1>
-        <h2>Somos el grupo 22</h2>
-        <h3> En la materia {nombreMateria}</h3>
-    </div>
+  <div>
+      <h1>Hola React </h1>
+      <h2>Somos el grupo 22</h2>
+      <h3> En la materia {nombreMateria}</h3>
+  </div>
 )
 }
 
